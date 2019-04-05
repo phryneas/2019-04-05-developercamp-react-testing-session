0a21862a70443045e6ec216ec4019f53d4472f72
Lenz Weber <lenz.weber@mayflower.de>
2019-04-05 19:03:01 +0200

    contents from session, use react-alpha to fix act behaviour

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { TextField } from "@material-ui/core";
 
-export function App() {
+export function App(props) {
+  const [username, setUsername] = React.useState("");
+
+  const [state, setState] = React.useState("loading");
+  React.useEffect(() => {
+    const timeout = setTimeout(() => setState("success"), 500);
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <>
       <h1>It is working!</h1>
-      <div>Hello World!</div>
+      <div>Hello {props.username}!</div>
+      <label>
+        Your Username:{" "}
+        <input
+          type="text"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+      </label>
+      <p>state: {state}</p>
     </>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,23 +1,56 @@
 import React from "react";
-import { render as rtlRender } from "react-testing-library";
+import {
+  render as rtlRender,
+  fireEvent,
+  wait,
+  act,
+  waitForElement
+} from "react-testing-library";
 import { App } from "./App";
+import "@babel/polyfill";
 
-function render({ Component = () => <App /> } = {}) {
-  return rtlRender(<Component />);
+function render({ component = <App /> } = {}) {
+  return rtlRender(component);
 }
 
 describe("basic rendering", () => {
   it("renders", () => {
     render();
   });
-
   it("says that it is working", () => {
     const { getByText } = render();
-    expect(getByText(/is working/i)).toBeInTheDocument();
+    expect(getByText(/it is working/i)).toBeInTheDocument();
+  });
+  it("prints 'hello <username>'", () => {
+    const { getByText } = render({
+      component: <App username="Tim" />
+    });
+    expect(getByText(/hello tim/i)).toBeInTheDocument();
+    expect(getByText(/Tim/)).toBeInTheDocument();
   });
 
-  it("also renders the text 'Hello World'", () => {
-    const { getByText } = render();
-    expect(getByText(/hello world/i)).toBeInTheDocument();
+  it("has a correctly labeled input", () => {
+    const { getByLabelText, debug } = render();
+    const input = getByLabelText(/username/i);
+    expect(input).toBeInTheDocument();
+
+    expect(input.value).toBe("");
+
+    fireEvent.change(input, { target: { value: "NeuerText" } });
+
+    expect(input.value).toBe("NeuerText");
+
+    // debug();
+  });
+
+  it("switches from loading to success", async () => {
+    const { getByText, queryByText } = render();
+    expect(getByText(/loading/i)).toBeInTheDocument();
+    expect(queryByText(/success/i)).not.toBeInTheDocument();
+
+    await act(() => waitForElement(() => getByText(/success/i)));
+
+    expect(getByText(/success/i)).toBeInTheDocument();
+    expect(queryByText(/loading/i)).not.toBeInTheDocument();
   });
 });
